d9f12332342ee4efa7a65aa31eb758dbf0bd95be
Maia Kapur <kapurm@uw.edu>
2018-09-26 15:13:22 -0700

    comment out pnorms - this works

--- sptlvb.cpp
+++ sptlvb.cpp
@@ -6,11 +6,16 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // load data; each column is a strata
-  DATA_MATRIX(Length_cm);
+  DATA_MATRIX(Length_cm); //  may have truncated data
   DATA_MATRIX(Age);
+  
+  // DATA_MATRIX(len_unif); // full dataset
+  // DATA_MATRIX(age_unif); // full dataset
+  
   DATA_VECTOR(minSel); // obtained via 95%CI from raw data
   DATA_VECTOR(maxSel);
   DATA_INTEGER(nStrata);
+  DATA_INTEGER(selType);
 
   int n = 500; // for now, need to change this to ,size() or something
   
@@ -37,26 +42,31 @@ Type objective_function<Type>::operator() ()
   
   for(int j = 0; j < nStrata; j++){     // iterate strata columns
     for(int i = 0; i < n; i++){ // iterate rows
-      yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j) - t0(j)))); // return single value
-      ypreds(i,j) = yfit;
-      unif_obj_fun -= dnorm(Length_cm(i,j), yfit, Sigma, true); // uniform selectivity
+      yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j) - t0(j)))); //use full data set
       
-      switch( selType ){
-      case 1 : // uniform selectivity(no correction)
-      obj_fun -= unif_obj_fun; // uniform selectivity
-        
-      case 2 : // minimum truncation
-        trunc_fun = 1-
-        obj_fun = unif_obj_fun/trunc_fun;
-        break;
-      case 3 : // maximum truncation
-        obj_fun = unif_obj_fun/trunc_fun;
-        
-        break;
-      case 4 : // dome shaped selectivity
-        obj_fun = unif_obj_fun/trunc_fun;
-      break;
-       }
+      unif_obj_fun -= dnorm(Length_cm(i,j), yfit, Sigma, true); // uniform selectivity
+      // switch( selType ){
+      // case 1 : // uniform selectivity(no correction)
+        obj_fun = unif_obj_fun;
+        ypreds(i,j) = yfit; // for plotting
+      // case 2 : // minimum truncation
+      //   yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j) - t0(j)))); // return single value
+      //   // trunc_fun = 1-pnorm(minSel(j), yfit, Sigma); //not logged yet
+      //   // obj_fun -= unif_obj_fun/log(trunc_fun);
+      //   obj_fun -= unif_obj_fun/log(3);
+      //   ypreds(i,j) = yfit; // for plotting
+      //   break;
+      // case 3 : // maximum truncation
+      //   yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j) - t0(j)))); // return single value
+      //   trunc_fun = pnorm(maxSel(i), yfit, Sigma, true);
+      //   obj_fun -= unif_obj_fun/trunc_fun;
+      //   break;
+      // case 4 : // dome shaped selectivity
+      //   trunc_fun = unif_obj_fun -  1-pnorm(minSel(i), yfit, Sigma, true) - pnorm(maxSel(i), yfit, Sigma, true);;
+      // 
+      //   obj_fun = unif_obj_fun/trunc_fun;
+        // break;
+      // }
     }
   }
   

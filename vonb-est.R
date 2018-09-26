4d6da306eba78dff99d1cd6548e0f5937ae77ec3
Maia Kapur <kapurm@uw.edu>
2018-09-26 11:54:18 -0700

    add in for loop

--- vonb-est.R
+++ vonb-est.R
@@ -6,48 +6,98 @@ load( paste0(getwd(),"/filtered_SAB_WC.rda")); load(paste0(getwd(),"/lenmat_WC.r
 nStrata <- length(unique(len$st_f))
 
 ## Estimation
-data <- list(Length_cm = lenmat, Age = agemat, st = len[,'st_f'], nStrata = nStrata )
-parameters <- list(dummy = 0, log_Linf = rep(log(70), nStrata), log_k = rep(0, nStrata), log_t0 = rep(0, nStrata), log_Sigma = 0.1)
+s <- c(1:4)[1] ## choose between uniform, min, max, or dome selectivity
+minSel <- c(42, 37, 39, 27, 23, 21) ## cutoff values for selectivity
+maxSel <- c(74, 70, 77, 71, 71, 63)
 
-compile("sptlvb.cpp")
-dyn.load(dynlib("sptlvb"))
-# Now estimate everything
-map <- NULL
-model <- MakeADFun(data, parameters,  DLL="sptlvb",silent=T,map=map)
 
-# Bounds on the parameters MAKE SURE THESE MATCH THE ORDER AND SIZE OF EACH PARAM
-# lowbnd = c( -10, -7, rep(0.1,m),rep(-7,m), rep(-Inf,m))
-# uppbnd = c( 10, 4, rep(1000,m),rep(4,m), rep(Inf,m))
+## a function to get 500 samples within selectivity bounds
+data.sub <- function(len, s){
+  ## reshape raw length and age data
+  agemat0 <-
+    len %>% select(Age, st) %>% mutate(ID = 1:nrow(.)) %>% melt(., id = c('st', 'ID'),'Age') %>% 
+    reshape::cast(., ID + value ~ st, fill = NA, drop= T) %>% select(-ID, -value) 
+  lenmat0 <- 
+    len %>% select(Length_cm, st) %>% mutate(ID = 1:nrow(.)) %>% melt(., id = c('st', 'ID'),'Length_cm') %>% 
+    reshape::cast(., ID + value ~ st, fill = NA, drop= T) %>% select(-ID, -value) 
+  
+  agemat <- lenmat <-  matrix(NA, nrow = 500, ncol = length(unique(len$st)))
+  
+  for(i in 1:nStrata){
+    temp0 <- matrix(na.omit(lenmat0[,i]),ncol = 1) ##if there is a value in one, should be in the other as well 
+    if(s == 1) unif <- temp <- temp0 ## used in all sims
+    if(s == 2) { temp <- temp0[temp0 >= minSel[i]]}
+    # if(s == 3) { temp <- temp[temp <= maxSel[i]]}  ## sample size issues
+    # if(s == 4) { temp <- temp[temp >= minSel[i] & temp <= maxSel[i]]} ## sample size issues
+    idx <- sample(1:length(temp),500) ## pick 500 rows and store index
+    lenmat[,i] <- temp[idx] %>% as.matrix()
+    agemat[,i] <- data.frame(na.omit(agemat0[,i]))[idx,] %>% as.matrix() ## select same rows
+    
+    lenmat[,i] <- unif[idx] %>% as.matrix()
+    agemat[,i] <- data.frame(na.omit(agemat0[,i]))[idx,] %>% as.matrix() ## select same rows
+    
+    rm(temp0)
+  }
+  return(list(lenmat,agemat))
+}
 
-fit <- nlminb(
-  model$par,
-  model$fn,
-  model$gr,
-  control = list(
-    rel.tol = 1e-12,
-    eval.max = 100000,
-    iter.max = 1000
+
+
+for(s in 1:2){ ## ultimately loop over 4 selectivities
+  matrices <- data.sub(len, s)
+  
+  data <-
+    list(
+      Length_cm = matrices[[1]], 
+      Age = matrices[[2]],
+      st = len[, 'st_f'],
+      nStrata = nStrata,
+      minSel = minSel,
+      maxSel = maxSel,
+      selType = s
+    )
+  parameters <-
+    list(
+      dummy = 0,
+      log_Linf = rep(log(70), nStrata),
+      log_k = rep(0, nStrata),
+      t0 = rep(0, nStrata),
+      log_Sigma = 0.1
+    )
+  
+  compile("sptlvb.cpp")
+  dyn.load(dynlib("sptlvb"))
+  # Now estimate everything
+  map <- NULL
+  model <- MakeADFun(data, parameters,  DLL="sptlvb",silent=T,map=map)
+  
+  fit <- nlminb(
+    model$par,
+    model$fn,
+    model$gr,
+    control = list(
+      rel.tol = 1e-12,
+      eval.max = 100000,
+      iter.max = 1000
+    )
   )
-  # ,
-  # lower = lowbnd,
-  # upper = uppbnd
-)
-best <- model$env$last.par.best
-print(exp(best))
-rep <- sdreport(model)
-print(exp(rep))
-
+  best <- model$env$last.par.best
+  print(c(best[1:6],exp(best[7:length(best)])))
+  rep <- sdreport(model)
+}
 ## plotting
+require(ggplot2);require(dplyr);require(reshape)
 dat0 <- model$report()$ypreds %>% data.frame() 
 names(dat0) <- paste0(unique(len$st_f))
-dat0 <- dat0 %>% melt() #%>% rename(c('value' = 'Predicted', 'variable' = 'st'))
-dat0$st_f = factor(dat0$variable, levels=c('deep_n','mid_n','shallow_n','deep_s', "mid_s","shallow_s"))
+dat0 <- dat0 %>% reshape::melt() %>% plyr::rename(c('value' = 'Predicted', 'variable' = 'st'))
+dat0$st_f <- factor(dat0$st, levels=c('deep_n','mid_n','shallow_n','deep_s', "mid_s","shallow_s")) 
 
-agedf <- agemat %>% data.frame() %>% melt() %>% rename(c('value' = 'Age'))
-lendf <- lenmat %>% data.frame() %>% melt() %>% rename(c('value' = 'Length'))
+agedf <- agemat %>% data.frame() %>% reshape::melt() %>% plyr::rename(c('value' = 'Age'))
+lendf <- lenmat %>% data.frame() %>% reshape::melt() %>% plyr::rename(c('value' = 'Length'))
 
-mdf <- bind_cols(dat0,agedf,lendf) %>% select(-variable1, -variable2)
-mdf$st_f = factor(mdf$variable, levels=c('deep_n','mid_n','shallow_n','deep_s', "mid_s","shallow_s"))
+mdf <- bind_cols(dat0,agedf,lendf) %>% select(-variable, -variable1, -st)
+# mdf$st_f = factor(mdf$variable, levels=c('deep_n','mid_n','shallow_n','deep_s', "mid_s","shallow_s"))
+# save(mdf,file =  paste0(getwd(),"/uniform_predicts.rda"))
 
 ggplot(mdf, aes(x = Age, y = Length)) +
   theme_minimal() +
@@ -58,7 +108,8 @@ ggplot(mdf, aes(x = Age, y = Length)) +
   geom_line(aes(y = Predicted), color = 'blue', lwd = 1.1)+
   facet_wrap(~ st_f) +
   labs(title = "Predicted Model Fits with Uniform Selectivity", y = 'Length (cm)', x= 'Age (yr)', subtitle = 'Points are actual subsampled data used in parameter fitting')
-ggsave(file = paste0(getwd(),"/plots/uniform_fits_sub.png"), plot = last_plot(), height = 5, width = 7, unit = 'in', dpi = 520)
+
+# ggsave(file = paste0(getwd(),"/plots/uniform_fits_sub.png"), plot = last_plot(), height = 5, width = 7, unit = 'in', dpi = 520)
 
 
 ggplot(len, aes(x = Age, y = Length_cm, color = Sex)) +
@@ -71,5 +122,5 @@ ggplot(len, aes(x = Age, y = Length_cm, color = Sex)) +
   geom_line(data = mdf, aes(x = Age, y = Predicted), col = 'blue', lwd = 1.1) +
   facet_wrap(~ st_f) +
   labs(title = "Predicted Model Fits with Uniform Selectivity", y = 'Length (cm)', x= 'Age (yr)', subtitle = 'Parameters estimated using subset of each strata')
-  
-ggsave(file = paste0(getwd(),"/plots/uniform_fits.png"), plot = last_plot(), height = 5, width = 7, unit = 'in', dpi = 520)
+
+# ggsave(file = paste0(getwd(),"/plots/uniform_fits.png"), plot = last_plot(), height = 5, width = 7, unit = 'in', dpi = 520)

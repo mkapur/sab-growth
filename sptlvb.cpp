b018c0c750b7789232bf9651882332f32fa5b4df
Maia Kapur <kapurm@uw.edu>
2018-10-12 09:55:04 -0700

    nmat as matrix

--- sptlvb.cpp
+++ sptlvb.cpp
@@ -14,7 +14,7 @@ Type objective_function<Type>::operator() ()
   DATA_VECTOR(maxSel);
   DATA_INTEGER(nStrata);
   DATA_INTEGER(selType);
-  DATA_VECTOR(nmat);
+  DATA_MATRIX(nmat);
   
   // things to estimate (per stratum)
   PARAMETER_VECTOR(t0); // t0 can be negative 
@@ -28,7 +28,7 @@ Type objective_function<Type>::operator() ()
   
   vector<Type> Linf(nStrata);
   Linf = exp(log_Linf);
-
+  
   // vector<Type> log_minSel(nStrata);
   // log_minSel = exp(minSel);
   
@@ -38,9 +38,9 @@ Type objective_function<Type>::operator() ()
   // vector<Type> n(nStrata);
   Type Sigma = exp(log_Sigma); // assuming universal sigma for now, later vectorize
   Type yfit = 0;
-
+  
   // matrix<Type> ypreds(8481,nStrata); // used for plotting later
-  // array<Type> ypreds(8481,nStrata,2); // used for plotting later
+  array<Type> ypreds(34069,nStrata,2); // used for plotting later
   
   Type obj_fun = 0.0;
   Type unif_obj_fun = 0.0; // used as numerator in all simulations
@@ -49,36 +49,35 @@ Type objective_function<Type>::operator() ()
   for(int s = 0; s < 2; s++) // iterate sexes
     for(int j = 0; j < nStrata; j++){     // iterate strata columns
       for(int i = 0; i < nmat(j,s); i++){ // iterate rows, unique to strata
-        // yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j,s) - t0(j)))); 
-        // unif_obj_fun = dnorm(Length_cm(i,j,s), yfit, Sigma);
-        unif_obj_fun = dnorm(Type(56.6), yfit, Sigma);
+        yfit = Linf(j)*(1-exp(-k(j)*(Age(i,j,s) - t0(j))));
+        unif_obj_fun = dnorm(Length_cm(i,j,s), yfit, Sigma);
         switch( selType ){
         case 1 : // uniform selectivity(no correction)
           obj_fun -= log(unif_obj_fun);
-          // ypreds(i,j,s) = yfit;
+          ypreds(i,j,s) = yfit;
           break;
-        case 2 : // minimum truncation
-          trunc_fun = 1-pnorm(minSel(j), yfit, Sigma); 
-          obj_fun -= unif_obj_fun/log(trunc_fun);
-          // ypreds(i,j,s) = yfit;
-          break;
-        case 3 : // maximum truncation
-          trunc_fun = unif_obj_fun/pnorm(maxSel(j), yfit, Sigma);
-          obj_fun -= log(trunc_fun);
-          // ypreds(i,j,s) = yfit;
-          break;
+        // case 2 : // minimum truncation
+        //   trunc_fun = 1-pnorm(minSel(j), yfit, Sigma); 
+        //   obj_fun -= unif_obj_fun/log(trunc_fun);
+        //   // ypreds(i,j,s) = yfit;
+        //   break;
+        // case 3 : // maximum truncation
+        //   trunc_fun = unif_obj_fun/pnorm(maxSel(j), yfit, Sigma);
+        //   obj_fun -= log(trunc_fun);
+        //   // ypreds(i,j,s) = yfit;
+        //   break;
         case 4 : // dome shaped selectivity
           trunc_fun = pnorm(maxSel(j), yfit, Sigma) - pnorm(minSel(j), yfit, Sigma) ;
           obj_fun = unif_obj_fun/log(trunc_fun);
-          // ypreds(i,j,s) = yfit;
+          ypreds(i,j,s) = yfit;
           break;
         }
       }
     }
     
-    // REPORT(ypreds);
+    REPORT(ypreds);
     REPORT(yfit);
-  REPORT(trunc_fun)
+  REPORT(trunc_fun);
   ADREPORT(Linf);
   ADREPORT(k);
   ADREPORT(t0);

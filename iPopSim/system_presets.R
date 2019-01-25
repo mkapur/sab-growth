2b80e868e0b9fd58cddc0bd7fc6b2691cfe3f2b5
mkapur <kapur.mr@gmail.com>
2019-01-24 17:46:16 -0800

    change system presets to deal with age and working on fhist

--- iPopSim/system_presets.R
+++ iPopSim/system_presets.R
@@ -96,8 +96,11 @@ Inst_M = 0.25
 #catch_sampling_rate = 0.9 
 
 # selectivity ogive
-SEL_50 = 147
-SEL_95 = 176
+# SEL_50 = 147 
+# SEL_95 = 176
+## ages -- eyeballed from plot!
+SEL_50 <- 2
+SEL_95 <- 4
 
 # fishery-dependent catchability
 fishery_q = 0.00001
@@ -122,8 +125,12 @@ OBS_age_bin = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 Nsamp_Mean_length_at_age = 20
 
 # selectivity FUNCTION
-selectivity  = function(fish_size){
-  y=1/(1+exp(-log(19)*(fish_size-SEL_50)/(SEL_95-SEL_50)))
+# selectivity  = function(fish_size){
+#   y=1/(1+exp(-log(19)*(fish_size-SEL_50)/(SEL_95-SEL_50)))
+#   y
+# }
+selectivity  = function(true_age){
+  y=1/(1+exp(-log(19)*(true_age-SEL_50)/(SEL_95-SEL_50)))
   y
 }
 
@@ -131,9 +138,9 @@ test_fish_size = c(0:400)
 
 ## Effort Simulator ----
 generate.effort = function(level,nsim) {
-  
-  getFhist<-function(nsim,Esd,nyears,dFmin,dFmax,bb){
-    
+  # 
+ 
+  getFhist<-function(nsim,Esd,nyears,dFmin,dFmax,bb,scale){
     ne<-nsim*10                                           # Number of simulated effort datasets
     dEfinal<-runif(ne,dFmin,dFmax)                        # Sample the final gradient in effort
     a<-(dEfinal-bb)/nyears                                # Derive slope to get there from intercept
@@ -146,7 +153,9 @@ generate.effort = function(level,nsim) {
     for(y in 2:nyears){
       E[,y]<-apply(dE[,1:y],1,sum)
     }
-    E<-E/array(apply(E,1,mean),dim=c(ne,nyears))          # Standardise Effort to average 1
+    
+    E<-scale*E/array(apply(E,1,mean),dim=c(ne,nyears))          # Standardise Effort to average 1
+    
     cond<-apply(E,1,min)>0
     pos<-(1:ne)[cond]
     pos<-pos[1:nsim]
@@ -157,37 +166,19 @@ generate.effort = function(level,nsim) {
     E*Eerr
   }
   ## create a data frame based on your number of sims and desired fishing effort. Add in an extra year.
+  
+  
   df <- data.frame(
     'SIM' = rep(1:nsim, each = F_start_yr+1),
     'YEAR' = rep(seq(1, F_start_yr+1, 1), nsim),
     'FMORT' = if (level == 'HIGH') {
-      as.vector(getFhist(
-        nsim = nsim,
-        Esd = 0,
-        nyears = F_start_yr+1, 
-        dFmin = -0.1,
-        dFmax = 0.1,
-        bb = 0.15
-      ))
+      as.vector(getFhist(nsim=1,Esd=0.001,nyears=F_start_yr+1,dFmin=-0.07,dFmax=0.07,bb=-0.1,scale = 0.3/1.323))
     }
     else if (level == 'MED') {
-      as.vector(getFhist(
-        nsim = nsim,
-        Esd = 0,
-        nyears = F_start_yr+1,
-        dFmin = -0.1,
-        dFmax = 0.1,
-        bb = 0.25
-      ))
+      as.vector(getFhist(nsim=1,Esd=0.001,nyears=F_start_yr+1,dFmin=-0.07,dFmax=0.07,bb=-0.1,scale = 0.2/1.323))
     } else {
-      as.vector(getFhist(
-        nsim = nsim,
-        Esd = 0,
-        nyears =  F_start_yr+1,
-        dFmin = -0.15,
-        dFmax = 0.15,
-        bb = 0.35
-      ))
+      as.vector(getFhist(nsim=1,Esd=0.001,nyears=F_start_yr+1,dFmin=-0.07,dFmax=0.07,bb=-0.1,scale = 0.1/1.323))
+      
     }
   )
   write.csv(df,
@@ -240,3 +231,64 @@ maketables = function(path_name){
   #             row.names = FALSE,col.names = TRUE,sep=",")
 } ## end of function
 
+## Deprecated
+
+## old Fgen was having issues and didn't use scale ----
+# getFhist<-function(nsim,Esd,nyears,dFmin,dFmax,bb){
+#   
+#   ne<-nsim*10                                           # Number of simulated effort datasets
+#   dEfinal<-runif(ne,dFmin,dFmax)                        # Sample the final gradient in effort
+#   a<-(dEfinal-bb)/nyears                                # Derive slope to get there from intercept
+#   a<-array(a,dim=c(ne,nyears))                          # Slope array
+#   bb<-array(bb,dim=c(ne,nyears))                        # Intercept array
+#   x<-array(rep(1:nyears,each=ne),dim=c(ne,nyears))      # Year array
+#   dE<-a*x+bb                                            # Change in effort
+#   E<-array(NA,dim=c(ne,nyears))                         # Define total effort array
+#   E[,1]<-dE[,1]
+#   for(y in 2:nyears){
+#     E[,y]<-apply(dE[,1:y],1,sum)
+#   }
+#   E<-E/array(apply(E,1,mean),dim=c(ne,nyears))          # Standardise Effort to average 1
+#   cond<-apply(E,1,min)>0
+#   pos<-(1:ne)[cond]
+#   pos<-pos[1:nsim]
+#   
+#   E<-E[pos,]                                            # Sample only those without negative effort
+#   Emu<--0.5*Esd^2
+#   Eerr<-array(exp(rnorm(nyears*nsim,rep(Emu,nyears),rep(Esd,nyears))),c(nsim,nyears))
+#   E*Eerr
+# # }
+# df <- data.frame(
+#   'SIM' = rep(1:nsim, each = F_start_yr+1),
+#   'YEAR' = rep(seq(1, F_start_yr+1, 1), nsim),
+#   'FMORT' = if (level == 'HIGH') {
+#     as.vector(getFhist(
+#       nsim = nsim,
+#       Esd = 0,
+#       nyears = F_start_yr+1, 
+#       dFmin = -0.1,
+#       dFmax = 0.1,
+#       bb = 0.15
+#     ))
+#   }
+#   else if (level == 'MED') {
+#     as.vector(getFhist(
+#       nsim = nsim,
+#       Esd = 0,
+#       nyears = F_start_yr+1,
+#       dFmin = -0.1,
+#       dFmax = 0.1,
+#       bb = 0.25
+#     ))
+#   } else {
+#     as.vector(getFhist(
+#       nsim = nsim,
+#       Esd = 0,
+#       nyears =  F_start_yr+1,
+#       dFmin = -0.15,
+#       dFmax = 0.15,
+#       bb = 0.35
+#     ))
+#   }
+# )
+# 

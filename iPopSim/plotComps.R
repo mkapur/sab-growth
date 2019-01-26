b670888d5450bed5adfcb18b41c536165d2ae372
Maia Kapur <kapurm@uw.edu>
2019-01-26 12:44:17 -0800

    building

---
+++ iPopSim/plotComps.R
@@ -0,0 +1,98 @@
+build_simComp <- function(out_file, dat0 = NA){
+  
+  if(is.na(dat0)){
+  ## identify boot directories for this F run
+  boots  <- list.dirs(out_file, recursive = T)[grep('boot',list.dirs(out_file, recursive = T))]
+  
+  ## bind all rows
+  dat <- list.files(boots, full.names = T)[grep('SAA',list.files(boots, full.names = T))] %>%
+    map(read.table,sep=",",header=T) %>%
+    reduce(bind_rows) 
+  
+  ## make summary dfs and plot 
+  dat0 <- dat %>%
+    sample_n(10000) 
+  
+  ## extract interesting columns and save 
+  dat0 %>% 
+    mutate(Length_cm = fish_size/10, 
+           block = ifelse(Year < 75, 'early','late'),
+           Flev = tolower(ifelse(block == 'early',
+                                 strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2]))) %>% 
+    select(Year, Age, AgeE, Length_cm, FSIM, block, Flev, REG) %>% 
+    write.csv(., paste0(out_file,"/",tolower(paste0(unique(dat$FSIM))),"sim_Comp.csv"),row.names = F)
+
+  }
+  
+  ## for age comps (true values only, aging error is not binnable)
+  dat1 <- dat0 %>%
+    group_by(Year, Age) %>% 
+    summarise(n = n()) %>%
+    mutate(freq = n / sum(n)) %>% 
+    filter(Year %in% c(50:99)) %>%
+    mutate(block = ifelse(Year < 75, 'early','late'),
+           Flev = tolower(ifelse(block == 'early', strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2])))
+  
+  
+  ## length comps, use rounded vals for binning (only for illustration)
+  dat2 <- dat0 %>%
+    mutate(fish_size = round(fish_size)) %>%
+    group_by(Year, fish_size) %>% 
+    summarise(n = n()) %>%
+    mutate(freq = n / sum(n)) %>% 
+    filter(Year %in% c(50:99)) %>%
+    mutate(block = ifelse(Year < 75, 'early','late'),
+           Flev = tolower(ifelse(block == 'early', strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2])))
+  
+  
+  
+  datN <- dat1 %>% group_by(Year) %>% summarise(totN = sum(n))
+  
+
+  return(list(dat1,dat2,datN))
+  
+}
+
+
+plotComps <- function(dat1,dat2,datN, saveloc = NA){
+ 
+  
+  ## plot ages
+  ggplot(dat1, aes(x = Age, y = freq, fill= Flev)) +
+    theme_bw() +
+    theme(panel.grid = element_blank())+
+    geom_area(stat = 'identity', alpha = 0.5) +
+    geom_line()+
+    facet_wrap( ~ Year, ncol = 4) +
+    scale_x_continuous(limits = c(0,max(dat1$Age))) +
+    scale_y_continuous(breaks = seq(0,0.4,0.2),labels = seq(0,0.4,0.2)) +
+    annotate("text", x = 0.9*max(dat1$Age), y = 0.35, label = paste0("n = ",datN$totN)) +
+    labs(y = 'proportion', title = paste0("Age comps Last 25 Years") )+
+    scale_fill_brewer(palette = "Dark2", name = "F Level")
+  
+  if(is.na(saveloc)){
+    ggsave(last_plot(), file = paste0(getwd(),"/plots/",tolower(paste0((dat$FSIM)," ",unique(dat$REG),"_ageComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+  } else{
+    ggsave(last_plot(), file = paste0(out_file,"/",paste0(fLevs[l,'DESC'],"_ageComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
+  }
+  
+  ## plot lengths
+  ggplot(dat2, aes(x = fish_size, y = freq, fill= Flev)) +
+    theme_bw() +
+    theme(panel.grid = element_blank())+
+    geom_area(stat = 'identity', alpha = 0.5) +
+    geom_line()+
+    facet_wrap( ~ Year, ncol = 4) +
+    scale_x_continuous(limits = c(50,250)) +
+    scale_y_continuous(breaks = seq(0,0.25,0.1),labels = seq(0,0.25,0.1)) +
+    annotate("text", x = 0.9*250, y = 0.1, label = paste0("n = ",datN$totN)) +
+    labs(y = 'proportion', x= "Length (mm)", title = paste0('Length comps Last 25 Years'))+
+    scale_fill_brewer(palette = "Dark2", name = "F Level")
+  
+  if(is.na(saveloc)){
+    ggsave(last_plot(), file = paste0(getwd(),"/plots/",tolower(paste0(unique(dat$FSIM), " ",unique(dat$REG),"_lenComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+  } else{
+    ggsave(last_plot(), file = paste0(out_file,"/",paste0(fLevs[l,'DESC'],"_lenComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
+  }  
+  cat("saved age and length comp plots \n")
+  }

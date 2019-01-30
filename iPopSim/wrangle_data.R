7ff0bf444415f81563a2eb8232c4d79e773cde59
mkapur <kapur.mr@gmail.com>
2019-01-29 17:20:44 -0800

    switch to scenID for fsim naming regime

--- iPopSim/wrangle_data.R
+++ iPopSim/wrangle_data.R
@@ -1,4 +1,4 @@
-build_simComp <- function(out_file, dat0 = NA){
+build_simComp <- function(out_file, l=l, dat0 = NA){
   
   if(is.na(dat0)){
   ## identify boot directories for this F run
@@ -12,26 +12,43 @@ build_simComp <- function(out_file, dat0 = NA){
   ## make summary dfs and plot 
   dat0 <- dat %>%
     sample_n(10000) 
-  
-  ## extract interesting columns and save 
+  scenID <-
+    ifelse(is.na(fLevs[l, 5]),
+           paste(fLevs[l, 3], fLevs[l, 4], sep = "_"),
+           paste(fLevs[l, 3], fLevs[l, 4], fLevs[l, 5],  sep = "_"))  ## extract interesting columns and save 
   dat0 %>% 
-    mutate(Length_cm = fish_size/10, 
-           block = ifelse(Year < 75, 'early','late'),
-           Flev = tolower(ifelse(block == 'early',
-                                 strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2]))) %>% 
-    select(Year, Age, AgeE, Length_cm, FSIM, FMORT, block, Flev, REG) %>% 
-    write.csv(., paste0(out_file,"/",tolower(paste0(unique(dat$FSIM))),"sim_Comp.csv"),row.names = F)
+    mutate(Length_cm = fish_size/10) %>% 
+    select(Year, Age, AgeE, Length_cm, FSIM, FMORT, REG) %>% 
+    write.csv(., paste0(out_file,"/",scenID,"sim_Comp.csv"),row.names = F)
 
   }
   
   ## for age comps (true values only, aging error is not binnable)
+  scenID <-
+    ifelse(is.na(fLevs[l, 5]),
+           paste(fLevs[l, 3], fLevs[l, 4], sep = "_"),
+           paste(fLevs[l, 3], fLevs[l, 4], fLevs[l, 5],  sep = "_"))  
+  
   dat1 <- dat0 %>%
     group_by(Year, Age) %>% 
     summarise(n = n()) %>%
     mutate(freq = n / sum(n)) %>% 
-    filter(Year %in% c(50:99)) %>%
-    mutate(block = ifelse(Year < 75, 'early','late'),
-           Flev = tolower(ifelse(block == 'early', strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2])))
+    filter(Year %in% c(50:99))
+  
+  dat1$Flev <- NA
+  
+  
+  
+  if(is.na(fLevs[l,5])){ ## if in 25 year blocks fill both
+    dat1$Flev[dat1$Year %in% 0:49] <- "ZERO"
+    dat1$Flev[dat1$Year %in% 50:75] <- as.character(fLevs[l,'FMORT_1'])
+    dat1$Flev[dat1$Year %in% 76:101]<- as.character(fLevs[l,'FMORT_2'])
+  } else {
+    dat1$Flev[dat1$Year %in% 0:49] <- "ZERO"
+    dat1$Flev[dat1$Year %in% 50:67] <- as.character(fLevs[l,'FMORT_1'])
+    dat1$Flev[dat1$Year %in% 68:85]<- as.character(fLevs[l,'FMORT_2'])
+    dat1$Flev[dat1$Year %in% 86:101]<- as.character(fLevs[l,'FMORT_3'])
+  }
   
   
   ## length comps, use rounded vals for binning (only for illustration)
@@ -40,23 +57,33 @@ build_simComp <- function(out_file, dat0 = NA){
     group_by(Year, fish_size) %>% 
     summarise(n = n()) %>%
     mutate(freq = n / sum(n)) %>% 
-    filter(Year %in% c(50:99)) %>%
-    mutate(block = ifelse(Year < 75, 'early','late'),
-           Flev = tolower(ifelse(block == 'early', strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2])))
-  
+    filter(Year %in% c(50:99)) #%>%
+    # mutate(block = ifelse(Year < 75, 'early','late'),
+    #        Flev = tolower(ifelse(block == 'early', strsplit(as.character(dat$FSIM[1])," ")[[1]][1], strsplit(as.character(dat$FSIM[1])," ")[[1]][2])))
+  dat2$Flev <- NA
+  if(is.na(fLevs[l,5])){ ## if in 25 year blocks fill both
+    dat2$Flev[dat2$Year %in% 0:49] <- "ZERO"
+    dat2$Flev[dat2$Year %in% 50:75] <- as.character(fLevs[l,'FMORT_1'])
+    dat2$Flev[dat2$Year %in% 76:101]<- as.character(fLevs[l,'FMORT_2'])
+  } else {
+    dat2$Flev[dat2$Year %in% 0:49] <- "ZERO"
+    dat2$Flev[dat2$Year %in% 50:67] <- as.character(fLevs[l,'FMORT_1'])
+    dat2$Flev[dat2$Year %in% 68:85]<- as.character(fLevs[l,'FMORT_2'])
+    dat2$Flev[dat2$Year %in% 86:101]<- as.character(fLevs[l,'FMORT_3'])
+  }
   
   
   datN <- dat1 %>% group_by(Year) %>% summarise(totN = sum(n))
   
 
-  return(list(dat1,dat2,datN))
+  return(list(dat1,dat2,datN,scenID))
   
 }
 
 
-plotComps <- function(dat1,dat2,datN, saveloc = NA){
+plotComps <- function(dat1,dat2,datN,scenID, saveloc = NA){
  
-  
+
   ## plot ages
  p <- ggplot(dat1, aes(x = Age, y = freq, fill= Flev)) +
     theme_bw() +
@@ -67,13 +94,13 @@ plotComps <- function(dat1,dat2,datN, saveloc = NA){
     scale_x_continuous(limits = c(0,max(dat1$Age))) +
     scale_y_continuous(breaks = seq(0,0.4,0.2),labels = seq(0,0.4,0.2)) +
     annotate("text", x = 0.9*max(dat1$Age), y = 0.35, label = paste0("n = ",datN$totN)) +
-    labs(y = 'proportion', title = paste0("Age comps Last 25 Years") )+
+    labs(y = 'proportion', title = paste0("Age comps") )+
     scale_fill_brewer(palette = "Dark2", name = "F Level")
   
   if(is.na(saveloc)){
-    ggsave(p, file = paste0(getwd(),"/plots/",tolower(paste0(fLevs[l,1],fLevs[l,2],"_ageComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+    ggsave(p, file = paste0(getwd(),"/plots/",paste0(scenID,"_ageComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
   } else{
-    ggsave(p, file = paste0(out_file,"/",tolower(paste0(fLevs[l,1],fLevs[l,2], "_ageComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+    ggsave(p, file = paste0(out_file,"/",paste0(scenID, "_ageComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
   }
   
   ## plot lengths
@@ -83,16 +110,16 @@ plotComps <- function(dat1,dat2,datN, saveloc = NA){
     geom_area(stat = 'identity', alpha = 0.5) +
     geom_line()+
     facet_wrap( ~ Year, ncol = 4) +
-    scale_x_continuous(limits = c(50,250)) +
+    # scale_x_continuous(limits = c(50,250)) +
     # scale_y_continuous(breaks = seq(0,0.25,0.1),labels = seq(0,0.25,0.1)) +
-    annotate("text", x = 0.9*250, y = 0.1, label = paste0("n = ",datN$totN)) +
-    labs(y = 'proportion', x= "Length (mm)", title = paste0('Length comps Last 25 Years'))+
+    annotate("text", x = max(dat2$fish_size)*9, y = 0.1, label = paste0("n = ",datN$totN)) +
+    labs(y = 'proportion', x= "Length (mm)", title = paste0('Length comps'))+
     scale_fill_brewer(palette = "Dark2", name = "F Level")
   
   if(is.na(saveloc)){
-    ggsave(p, file = paste0(getwd(),"/plots/",tolower(paste0(fLevs[l,1],fLevs[l,2],"_lenComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+    ggsave(p, file = paste0(getwd(),"/plots/",paste0(scenID,"_lenComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
   } else{
-    ggsave(p, file = paste0(out_file,"/",tolower(paste0(fLevs[l,1],fLevs[l,2],"_lenComps.jpg"))), dpi = 480, height = 10, width = 8, unit = 'in')
+    ggsave(p, file = paste0(out_file,"/",paste0(scenID,"_lenComps.jpg")), dpi = 480, height = 10, width = 8, unit = 'in')
   } 
 }
  

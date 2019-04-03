d36a7c195e601ae196a7d478236e733488a6a35a
mkapur <kapur.mr@gmail.com>
2019-04-03 15:25:21 -0700

    source fitMod, accurate denoms for tabulation :)

--- simgam_Master.R
+++ simgam_Master.R
@@ -8,8 +8,8 @@ library(gridExtra); library(grid); library(lattice)
 
 compile("sptlvb.cpp") ## will throw 'incomplete line' msg, ignore
 dyn.load(dynlib("sptlvb"))
-compile("sptlschnute.cpp") 
-dyn.load(dynlib("sptlschnute"))
+# compile("sptlschnute.cpp") 
+# dyn.load(dynlib("sptlschnute"))
 
 compname <- c("Maia Kapur","mkapur")[1]
 
@@ -17,7 +17,7 @@ compname <- c("Maia Kapur","mkapur")[1]
 scenarios <- read.csv("./input_data/scenarios.csv",na.strings = 'NA') ## manual file
 age <- 6; nboot <- 100; testrows <- unique(scenarios$DESC) 
 # source("./functions/bootBreaks.R") ## about 10 mins -- don't need to do this >1x
-source("./functions/getGR.R")
+source("./functions/getGR.R");source("./functions/fitMod.R")
 # now loop boots and fit VB to get coverage probs
 ldfprop <-  read.csv( paste0("./GAM_output/ldf_raw_a6.csv"))
 cdf <- data.frame();idx <-1 ## storage coverage prob totals, rbind each scen
@@ -50,34 +50,45 @@ for(l in 1:length(unique(ldfprop$scen))){
         nStrata = nStrata
       )
     
+    # parameters <-
+    #   list(
+    #     log_Lone = rep(log(60), nStrata),
+    #     log_Ltwo = rep(log(275), nStrata),
+    #     log_k = rep(log(0.3), nStrata),
+    #     t0 = rep(0.1, nStrata),
+    #     log_Sigma = 0
+    #   )
+    
     parameters <-
       list(
-        log_Lone = rep(log(60), nStrata),
-        log_Ltwo = rep(log(275), nStrata),
+        log_Linf = rep(log(150), nStrata),
         log_k = rep(log(0.3), nStrata),
         t0 = rep(0.1, nStrata),
         log_Sigma = 0
       )
     
     # Now estimate everything -- DO PHASES
-    # map <- NULL
-    map <- list(log_Lone = rep(factor(NA),nStrata)) ## get K, other stuff shouldn't change much
+    map <- NULL
+    mod <- fitMod(data, parameters, modversion = 'sptlvb',map)
+    dat0 <- mod[[1]]
+    rep0 <- mod[[2]]
+    # map <- list(log_Lone = rep(factor(NA),nStrata)) ## get K, other stuff shouldn't change much
     # model <- MakeADFun(data, parameters,  DLL="sptlvb",silent=T,map=map)
-    phase1 <- fitMod(data, parameters, modversion = 'sptlschnute',map)[[2]]
-    parameters <-
-      list(
-        log_Lone = rep(log(60), nStrata),
-        log_Ltwo = log(phase1[phase1$variable == 'Ltwo','value']),
-        log_k = rep(log(0.3), nStrata),
-        t0 = rep(0.1, nStrata),
-        log_Sigma = 0
-      )
-    map <- list(log_Ltwo = rep(factor(NA),nStrata)) ## get K, other stuff shouldn't change much
-    phase2 <- fitMod(data, parameters, modversion = 'sptlschnute',map)
-    
-    dat0 <- phase2[[1]]
-    rep0 <-phase2[[2]]
-    rep0[rep0$variable == 'Ltwo','sd'] <-     phase1[phase1$variable == 'Ltwo','sd']
+    # phase1 <- fitMod(data, parameters, modversion = 'sptlvb',map)[[2]]
+    # parameters <-
+    #   list(
+    #     log_Lone = rep(log(60), nStrata),
+    #     log_Ltwo = log(phase1[phase1$variable == 'Ltwo','value']),
+    #     log_k = rep(log(0.3), nStrata),
+    #     t0 = rep(0.1, nStrata),
+    #     log_Sigma = 0
+    #   )
+    # map <- list(log_Ltwo = rep(factor(NA),nStrata)) ## get K, other stuff shouldn't change much
+    # phase2 <- fitMod(data, parameters, modversion = 'sptlschnute',map)
+    # 
+    # dat0 <- phase2[[1]]
+    # rep0 <-phase2[[2]]
+    # rep0[rep0$variable == 'Ltwo','sd'] <-     phase1[phase1$variable == 'Ltwo','sd']
 
     
    ## reformat outputs ----
@@ -96,7 +107,7 @@ for(l in 1:length(unique(ldfprop$scen))){
     ## plot estimates
     gamREGS <- factor(unique(dat$gamREG), levels=c("R1", "R2","R3","R4"))
     REGS <-  factor(unique(dat$REG), levels=c("R1", "R2","R3","R4")) ## what was used to GENERATE
-    parms <- c('Lone','Ltwo','k','Linf')[1:2]
+    parms <- c('L1','L2','k','Linf')[1:2]
     
     parest <-  read.csv(paste0("./output_data/",scen,"_parEst_gam_",b,"_",Sys.Date(),'.csv')) %>%
       filter(variable %in% parms) %>% mutate(source = 'Estimated')
@@ -112,8 +123,14 @@ for(l in 1:length(unique(ldfprop$scen))){
     for(iq in 1:length(unique(parest$REG))){ ## loop regions
       cdf[idx,'scen'] <- scen
       cdf[idx,'boot'] <- b
+      ## qualitative compare for regional
       cdf[idx,'gamREG'] <- if(!is.na(gamREGS[iq])){gamREGS[iq]}else{last(gamREGS)}
       cdf[idx,'REG'] <- if(!is.na(REGS[iq])){REGS[iq]}else{last(REGS)}
+      
+      ## direct comp for break locations
+      cdf[idx,'gamLAT'] <- breaksdf$lat_breaks
+      cdf[idx,'gamLON'] <- breaksdf$lon_breaks
+      
       ptf <- NULL
       for(v in  1:length(parms)){ ## loop variables
         tmp <- subset(parest, variable == parms[v] & source == 'Estimated'  | variable == parms[v] & source == 'Actual' & REG == cdf[idx,'REG'])
@@ -122,6 +139,14 @@ for(l in 1:length(unique(ldfprop$scen))){
           tmp$value[tmp$source == 'Actual'&  tmp$REG ==    cdf[idx,'REG']] <= bounds[2]
       } ## end vars
       cdf[idx,parms[1]] <- ptf[1];  cdf[idx,parms[2]] <- ptf[2]
+      cdf[idx,"LAT"] <- cdf[idx,"gamLAT"] == scenarios$SPATIAL[scenarios$DESC == cdf[idx,'scen']][1]
+      cdf[idx,"LON"] <- cdf[idx,"gamLON"] == scenarios$SPATIAL[scenarios$DESC == cdf[idx,'scen']][1]
+      if(cdf[idx,'scen']=="F0LMW")  { # won't match "overlap"
+        cdf[idx,"LAT"] <- cdf[idx,"gamLAT"] >= 20 & cdf[idx,"gamLAT"] <= 25
+        cdf[idx,"LON"] <- cdf[idx,"gamLON"] >= 20 & cdf[idx,"gamLON"] <= 25
+      }
+
+      
       idx <- idx+1
     } ## end gamREGS
     idx <- idx+1
@@ -129,22 +154,32 @@ for(l in 1:length(unique(ldfprop$scen))){
   } ## end boots
 } ## end ldfrows
 
-cdf0 <- cdf %>% filter(!is.na(scen) & !is.na(Lone) & !is.na(Ltwo)) 
-cdf0 %>% write.csv(.,file = './gam_output/cdf_',Sys.date(),'.csv',row.names = F)
+cdf0 <- cdf %>% filter(!is.na(scen)) # & !is.na(L1) & !is.na(L2)) 
+cdf0 %>% write.csv(.,file = paste0('./gam_output/cdf_',Sys.Date(),'.csv'),row.names = F)
 # cdf <- cdf[,c(1:5,11)]
 # names(cdf)[5:6] <- c('k','Linf')
 
 ## When did regional designation go right? (original analysis)
-cdfaccu <- cdf0 %>% group_by(scen) %>% summarise(propa = sum(gamREG == REG)/n())
-write.csv(cdfaccu,file = './gam_output/cdf_accu_',Sys.date(),'.csv',row.names = F)
+cdfaccu <- cdf0 %>% 
+  select(scen, LAT, LON) %>% 
+  melt(id = c('scen')) %>%
+  group_by(scen,variable) %>%
+  dplyr::summarise(denom = n(), n = sum(value)) %>% 
+  # dplyr::summarise(denom = n(), n = sum(gamREG == REG)) %>% 
+  mutate(prop = n/denom)
+# summarise(propa = sum(gamREG == REG)/n())
+write.csv(cdfaccu,file = paste0('./gam_output/cdf_accu_',Sys.Date(),'.csv'),row.names = F)
 
 ## Get coverage probs -- for all params. 
-## Note N varies because of varying # regions per boot. W
-## ill tabulate for k and linf indiscriminately, which is bad.
+## Note N varies because of varying # regions per boot. 
 cdfprop <- cdf0 %>% filter(!is.na(scen))  %>% 
-  melt(id = c('scen','boot','gamREG','REG')) %>% 
-  group_by(scen) %>% dplyr::summarise(n = sum(value)) %>% 
-  mutate(prop = n/nrow(.))
-write.csv(cdfprop,file = './gam_output/cdf_prop_',Sys.date(),'.csv',row.names = F)
+  # melt(id = c('scen','boot','gamREG','REG')) %>%
+  select(scen, L1, L2) %>% 
+  melt(id = c('scen')) %>%
+  # group_by(scen) %>%
+  group_by(scen,variable) %>%
+  dplyr::summarise(denom = n(), n = sum(value)) %>% 
+  mutate(prop = n/denom)
+write.csv(cdfprop,file = paste0('./gam_output/cdf_prop_',Sys.Date(),'.csv'),row.names = F)
 
 
